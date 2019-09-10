da6c6f2b8ee4002af6b5e40d6f7723bfcafc4584
HuongDo <huongdv7@fpt.com.vn>
2019-09-10 21:14:55 +0700

    effects

---
+++ src/app/routes.ts
@@ -0,0 +1,30 @@
+import { Routes } from '@angular/router';
+
+import { BookExistsGuard } from './guards/book-exists';
+import { FindBookPageComponent } from './containers/find-book-page';
+import { ViewBookPageComponent } from './containers/view-book-page';
+import { CollectionPageComponent } from './containers/collection-page';
+import { NotFoundPageComponent } from './containers/not-found-page';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: FindBookPageComponent
+  },
+  {
+    path: 'view',
+    component: CollectionPageComponent
+  },
+  {
+    path: 'book/find',
+    component: FindBookPageComponent
+  },
+  {
+    path: 'book/:id',
+    component: ViewBookPageComponent
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent
+  }
+];
